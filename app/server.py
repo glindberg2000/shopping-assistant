0a306d253a291f54e72162ec8350f0e71e07e0c7
Greg Lindberg <greglindberg@gmail.com>
2024-02-22 17:55:43 -0800

    files commit

---
+++ app/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from shopping_assistant.agent import agent_executor as shopping_assistant_chain
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+# Edit this to add the chain you want to add
+# add_routes(app, NotImplemented)
+add_routes(app, shopping_assistant_chain, path="/shopping-assistant")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
